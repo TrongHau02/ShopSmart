1dd36c9a48f64725f7837fbe06472183a13064d8
TrongHau02 <haunguyen.stmt@gmail.com>
2024-08-18 10:28:51 +0700

    Update project 3-tier layer

--- src/main/java/com/javaweb/controller/UserController.java
+++ src/main/java/com/javaweb/controller/UserController.java
@@ -1,22 +1,37 @@
 package com.javaweb.controller;
 
+import com.javaweb.domain.User;
+import com.javaweb.repository.UserRepository;
 import com.javaweb.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
-    private UserService userService;
-
+    private final UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric",test);
+        model.addAttribute("eric", "test");
+        return "hello";
+    }
+
+    @RequestMapping(value = "/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit ) {
+        System.out.println("run here " + hoidanit);
+        this.userService.handleSaveUser(hoidanit);
         return "hello";
     }
 }
