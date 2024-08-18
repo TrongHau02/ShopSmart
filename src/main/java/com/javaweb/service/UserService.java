a0aaabc7baa21687255fc5aa720a8df011684093
TrongHau02 <haunguyen.stmt@gmail.com>
2024-08-18 22:43:45 +0700

    Write method getUserById

--- src/main/java/com/javaweb/service/UserService.java
+++ src/main/java/com/javaweb/service/UserService.java
@@ -31,4 +31,8 @@ public class UserService {
     public List<User> getAllUsersByEmail(String email) {
         return this.userRepository.findOneByEmail(email);
     }
+
+    public User getUserById(long id) {
+        return this.userRepository.findById(id);
+    }
 }
