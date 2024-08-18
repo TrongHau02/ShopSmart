1e9b9ee6bf4cd875ec70639264eebe61b3370404
TrongHau02 <haunguyen.stmt@gmail.com>
2024-08-18 22:42:58 +0700

    add method findUserById

--- src/main/java/com/javaweb/repository/UserRepository.java
+++ src/main/java/com/javaweb/repository/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
     List<User> findOneByEmail(String email);
+    User findById(long id);
 }
