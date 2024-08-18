09b081b9de330b74bf67c34f8fe5564f0c3a6fa3
TrongHau02 <haunguyen.stmt@gmail.com>
2024-08-18 15:47:54 +0700

    Handle method get all Users

--- src/main/java/com/javaweb/service/UserService.java
+++ src/main/java/com/javaweb/service/UserService.java
@@ -4,6 +4,8 @@ import com.javaweb.domain.User;
 import com.javaweb.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -20,5 +22,13 @@ public class UserService {
         User newUser = this.userRepository.save(user);
         System.out.println(newUser);
         return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUsersByEmail(String email) {
+        return this.userRepository.findOneByEmail(email);
     }
 }
