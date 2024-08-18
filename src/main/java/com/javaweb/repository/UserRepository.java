5ee4dbe9e2c86f90a7fc63881cb05af710339394
TrongHau02 <haunguyen.stmt@gmail.com>
2024-08-18 15:47:14 +0700

    create method find User by Email

--- src/main/java/com/javaweb/repository/UserRepository.java
+++ src/main/java/com/javaweb/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.javaweb.repository;
 
 import com.javaweb.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
+    List<User> findOneByEmail(String email);
 }
