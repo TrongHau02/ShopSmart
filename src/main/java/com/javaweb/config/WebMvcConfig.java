f69dd608884047f1de9f2cc7ad3a70fa600c7e86
TrongHau02 <haunguyen.stmt@gmail.com>
2024-08-25 10:21:16 +0700

    add path to image folder

--- src/main/java/com/javaweb/config/WebMvcConfig.java
+++ src/main/java/com/javaweb/config/WebMvcConfig.java
@@ -3,7 +3,10 @@ package com.javaweb.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -29,5 +32,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
         registry.addResourceHandler("/assets/**").addResourceLocations("/resources/assets/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/resources/images/");
     }
 }
