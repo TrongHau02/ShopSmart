b172e2e842fb06d10e3edda16a9a6ff6872f0575
TrongHau02 <haunguyen.stmt@gmail.com>
2024-08-25 10:22:11 +0700

    create UploadService using upload File

---
+++ src/main/java/com/javaweb/service/UploadService.java
@@ -0,0 +1,40 @@
+package com.javaweb.service;
+
+import jakarta.servlet.ServletContext;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Service
+public class UploadService {
+    private ServletContext servletContext;
+
+    public UploadService(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
+
+    public String handleSaveUploadFile(MultipartFile file, String targetFolder) {
+        String finalName = "";
+        String rootPath = this.servletContext.getRealPath("/resources/images");
+        try {
+            byte[] bytes = file.getBytes();
+            File dir = new File(rootPath + File.separator + targetFolder);
+            if (!dir.exists())
+                dir.mkdirs();
+            // Create the file on server
+            finalName = System.currentTimeMillis() + "-" + file.getOriginalFilename();
+            File serverFile = new File(dir.getAbsolutePath() + File.separator + finalName);
+            BufferedOutputStream stream = new BufferedOutputStream(
+                    new FileOutputStream(serverFile));
+            stream.write(bytes);
+            stream.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return finalName;
+    }
+}
