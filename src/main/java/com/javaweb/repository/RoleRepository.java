6ebb3d853bb231ec2d4e477bc57726e9e45054c4
TrongHau02 <haunguyen.stmt@gmail.com>
2024-08-25 21:43:10 +0700

    Create new RoleRepository

---
+++ src/main/java/com/javaweb/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.javaweb.repository;
+
+import com.javaweb.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
