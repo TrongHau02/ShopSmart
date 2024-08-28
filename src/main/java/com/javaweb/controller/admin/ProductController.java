e3f9b39e5444af28695baaf2a5f754f5db9cbd07
TrongHau02 <haunguyen.stmt@gmail.com>
2024-08-28 09:19:22 +0700

    create method getCreateProductPage and createProduct

--- src/main/java/com/javaweb/controller/admin/ProductController.java
+++ src/main/java/com/javaweb/controller/admin/ProductController.java
@@ -1,12 +1,38 @@
 package com.javaweb.controller.admin;
 
+import com.javaweb.domain.Product;
+import com.javaweb.service.UploadService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class ProductController {
+    private final UploadService uploadService;
+
+    public ProductController(UploadService uploadService) {
+        this.uploadService = uploadService;
+    }
+
     @GetMapping(value = "/admin/product")
-    public String getHomePage() {
-        return "/admin/product/home";
+    public String getProductPage() {
+        return "admin/product/home";
+    }
+
+    @GetMapping(value = {"/admin/product/create"})
+    public String getCreateProductPage(Model model) {
+        model.addAttribute("newProduct", new Product());
+        return "admin/product/create";
+    }
+
+    @PostMapping(value = "/admin/product/create")
+    public String createProduct(@ModelAttribute("newProduct") Product product, @RequestParam("imageFile") MultipartFile file) {
+        String fileName = this.uploadService.handleSaveUploadFile(file, "product");
+        product.setImage(fileName);
+        return "redirect:/admin/product";
     }
 }
