21ce080a708207971bae11edfa45742174017799
TrongHau02 <haunguyen.stmt@gmail.com>
2024-09-01 17:22:09 +0700

    create function get all role

--- src/main/java/com/javaweb/service/RoleService.java
+++ src/main/java/com/javaweb/service/RoleService.java
@@ -5,6 +5,8 @@ import com.javaweb.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RoleService {
     private final RoleRepository roleRepository;
@@ -15,5 +17,9 @@ public class RoleService {
 
     public Role findRoleByName(String roleName) {
         return this.roleRepository.findByName(roleName);
+    }
+
+    public List<Role> findAll() {
+        return this.roleRepository.findAll();
     }
 }
