a6f7929aa5940c8add633ecc34598a0d2e9cacaa
TrongHau02 <haunguyen.stmt@gmail.com>
2024-09-01 22:18:20 +0700

    update funct get product with id

--- src/main/java/com/javaweb/controller/client/ProductController.java
+++ src/main/java/com/javaweb/controller/client/ProductController.java
@@ -1,5 +1,6 @@
 package com.javaweb.controller.client;
 
+import com.javaweb.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller(value = "productControllerOfClient")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping(value = "/product/{id}")
     public String getProductDetailById(@PathVariable("id") long id, Model model) {
         model.addAttribute("id", id);
+        model.addAttribute("productDetail", this.productService.findProductById(id));
         return "client/product/detail";
     }
 
