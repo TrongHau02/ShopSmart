430cb5de9a11b7c84c1c18169632f281c05031d5
TrongHau02 <haunguyen.stmt@gmail.com>
2024-09-01 17:26:04 +0700

    create model factory

---
+++ src/main/java/com/javaweb/domain/Factory.java
@@ -0,0 +1,38 @@
+package com.javaweb.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="factorys")
+public class Factory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String detailDesc;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetailDesc() {
+        return detailDesc;
+    }
+
+    public void setDetailDesc(String detailDesc) {
+        this.detailDesc = detailDesc;
+    }
+}
