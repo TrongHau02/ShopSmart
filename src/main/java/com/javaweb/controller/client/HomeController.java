2f381271e98fdb27115e7bf392e70aeb7c2af1f4
TrongHau02 <haunguyen.stmt@gmail.com>
2024-09-01 17:18:29 +0700

    Update function get UI home client

--- src/main/java/com/javaweb/controller/client/HomeController.java
+++ src/main/java/com/javaweb/controller/client/HomeController.java
@@ -1,12 +1,21 @@
 package com.javaweb.controller.client;
 
+import com.javaweb.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller(value = "homeControllerOfClient")
 public class HomeController {
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping(value = {"/", "/home"})
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        model.addAttribute("products", this.productService.findAllProduct());
         return "client/homepage/home";
     }
 }
