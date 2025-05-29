527785761a0b381cd0f948719a3c746f4d70751a
haung <haunguyen.stmt@gmail.com>
2025-05-29 10:26:21 +0700

    update url admin homepage

--- src/main/java/com/javaweb/controller/admin/HomeController.java
+++ src/main/java/com/javaweb/controller/admin/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller(value = "homeControllerOfAdmin")
 public class HomeController {
-    @GetMapping(value = {"/admin/home"})
+    @GetMapping(value = {"/admin"})
     public String getHomePage() {
         return "admin/dashboard/home";
     }
