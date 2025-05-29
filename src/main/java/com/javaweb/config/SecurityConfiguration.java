6d59a5f3ee04c46f189b8918344458f475475057
haung <haunguyen.stmt@gmail.com>
2025-05-29 10:25:37 +0700

    update login page with role admin

--- src/main/java/com/javaweb/config/SecurityConfiguration.java
+++ src/main/java/com/javaweb/config/SecurityConfiguration.java
@@ -45,6 +45,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE).permitAll()
                         .requestMatchers("/","/login", "/client/**", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
