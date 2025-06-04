ab7a1f65d11178b83a3b668114e7c4c63214cb48
haung <haunguyen.stmt@gmail.com>
2025-06-04 22:04:21 +0700

    add function addProductToCart

--- src/main/java/com/javaweb/controller/client/ProductController.java
+++ src/main/java/com/javaweb/controller/client/ProductController.java
@@ -1,10 +1,13 @@
 package com.javaweb.controller.client;
 
 import com.javaweb.service.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller(value = "productControllerOfClient")
 public class ProductController {
@@ -21,4 +24,11 @@ public class ProductController {
         return "client/product/detail";
     }
 
+    @PostMapping("/add-product-to-cart/{id}")
+    public String addProductToCart(@PathVariable("id") long productId, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        String email = (String) session.getAttribute("email");
+        this.productService.handleProductAddToCart(email, productId);
+        return "redirect:/";
+    }
 }
