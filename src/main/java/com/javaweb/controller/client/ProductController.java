92ba21ad449860732b615c60d93d69640301970e
haung <haunguyen.stmt@gmail.com>
2025-06-08 16:14:32 +0700

    update function getCartPage

--- src/main/java/com/javaweb/controller/client/ProductController.java
+++ src/main/java/com/javaweb/controller/client/ProductController.java
@@ -1,5 +1,8 @@
 package com.javaweb.controller.client;
 
+import com.javaweb.domain.Cart;
+import com.javaweb.domain.CartDetail;
+import com.javaweb.domain.User;
 import com.javaweb.service.ProductService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -8,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller(value = "productControllerOfClient")
 public class ProductController {
@@ -33,7 +38,20 @@ public class ProductController {
     }
 
     @GetMapping("/cart")
-    public String getCartPage() {
+    public String getCartPage(HttpServletRequest request, Model model) {
+        User currentUser = new User();
+        HttpSession session = request.getSession(false);
+        long id = (long) session.getAttribute("id");
+        currentUser.setId(id);
+        Cart cart = this.productService.fetchByUser(currentUser);
+        List<CartDetail> cartDetails = cart.getCartDetails();
+        double totalPrice = 0;
+        for (CartDetail cartDetail : cartDetails) {
+            totalPrice = cartDetail.getPrice() * cartDetail.getQuantity();
+        }
+        model.addAttribute("cartDetails", cartDetails);
+        model.addAttribute("totalPrice", totalPrice);
         return "client/cart/detail";
+
     }
 }
