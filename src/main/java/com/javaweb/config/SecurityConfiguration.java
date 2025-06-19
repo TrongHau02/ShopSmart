733eed9af78f43419a579faf15e2908e840eba7d
Nguyen Trong Hau <haunguyen.stmt@gmail.com>
2025-06-19 21:12:18 +0700

    update security

--- src/main/java/com/javaweb/config/SecurityConfiguration.java
+++ src/main/java/com/javaweb/config/SecurityConfiguration.java
@@ -60,7 +60,7 @@ public class SecurityConfiguration {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE).permitAll()
-                        .requestMatchers("/", "/login", "/client/**", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/", "/login", "/register", "/client/**", "/css/**", "/js/**", "/images/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .sessionManagement((sessionManagement) -> sessionManagement
